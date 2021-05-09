ca3f14b7dad6573591e39fa68536bdaa8abdf584
cgallonv <cgallonv@gmail.com>
2021-05-08 19:20:00 -0500

    Excepciones

---
+++ Java-Projects/Excepciones/src/excepciones1/Fallos.java
@@ -0,0 +1,19 @@
+package excepciones1;
+
+import javax.swing.*;
+
+public class Fallos {
+
+    public static void main(String[] args) {
+        int[] arrayint = {1,5,7,3,2};
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Posicion " + i + "=" + arrayint[i]);
+        }
+
+        String nombre = JOptionPane.showInputDialog("Ingresa tu nombre");
+        int edad = Integer.parseInt(JOptionPane.showInputDialog("Ingresa tu edad"));
+        System.out.println("Hola " + nombre + " Tienes " + edad + " años. Fin programa");
+    }
+
+
+}
